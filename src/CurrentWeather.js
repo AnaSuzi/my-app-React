928e795335801a280feac87a0cbd05135e173f66
AnaSuzi <104015770+AnaSuzi@users.noreply.github.com>
2022-06-14 10:29:09 +0200

    Made a new container CurrentWeather

---
+++ src/CurrentWeather.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./CurrentWeather.css";
+
+export default function CurrentWeather() {
+  return (
+    <div className="CurrentWeather">
+      <div className="container">
+        <div className="row justify-content-center first">
+          <div className="col-12 col-sm-6">
+            <h1>
+              <span className="place">
+                <i className="fa-solid fa-location-dot"></i>
+              </span>
+              <span className="cityNew" id="city-name"></span>
+            </h1>
+            <h2>
+              <div className="currentCity" id="current"></div>
+              <br />
+              <div className="temp-now">
+                <span id="max-temp">16</span>°<span id="min-temp">5</span>°
+              </div>
+              <br />
+              <div id="des" className="description"></div>
+            </h2>
+          </div>
+          <div className="col-12 col-sm-6">
+            <h3>
+              <span id="temp-number"></span>
+
+              <span id="celsius"> 20 °C </span>
+            </h3>
+
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              id="icon"
+              alt="neki"
+            />
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row situation">
+          <div className="col-3">
+            pressure
+            <br />
+            <strong>
+              <span id="pressure-number">1021</span>hPa
+            </strong>
+          </div>
+
+          <div className="col-3">
+            wind <br />
+            <strong>
+              <span id="wind-number">29</span>km/h
+            </strong>
+          </div>
+
+          <div className="col-3">
+            humidity <br />
+            <strong>
+              <span id="humidity-number">28</span>%
+            </strong>
+          </div>
+        </div>
+        <br />
+        <div className="weather-forecast" id="forecast"></div>
+      </div>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import SearchEngine from "./SearchEngine";
 import WeatherForecast from "./WeatherForecast";
+import CurrentWeather from "./CurrentWeather";
 
 export default function Weather() {
   return (
@@ -10,76 +11,15 @@ export default function Weather() {
         <div className="app-wrapper">
           <div className="row">
             <SearchEngine />
-            <div className="container">
-              <div className="row justify-content-center first">
-                <div className="col-12 col-sm-6">
-                  <h1>
-                    <span className="place">
-                      <i className="fa-solid fa-location-dot"></i>
-                    </span>
-                    <span className="cityNew" id="city-name"></span>
-                  </h1>
-                  <h2>
-                    <div className="currentCity" id="current"></div>
-                    <br />
-                    <div className="temp-now">
-                      <span id="max-temp">16</span>°<span id="min-temp">5</span>
-                      °
-                    </div>
-                    <br />
-                    <div id="des"></div>
-                  </h2>
-                </div>
 
-                <div className="col-12 col-sm-6">
-                  <h3>
-                    <span id="temp-number"> </span>
+            <CurrentWeather />
 
-                    <span id="celsius"> 20 °C </span>
-                  </h3>
-
-                  <img
-                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                    id="icon"
-                    alt="neki"
-                  />
-                </div>
-              </div>
-              <br />
-              <br />
-
-              <div className="row situation">
-                <div className="col-3">
-                  pressure
-                  <br />
-                  <strong>
-                    <span id="pressure-number">1021</span>hPa
-                  </strong>
-                </div>
-
-                <div className="col-3">
-                  wind <br />
-                  <strong>
-                    <span id="wind-number">29</span>km/h
-                  </strong>
-                </div>
-
-                <div className="col-3">
-                  humidity <br />
-                  <strong>
-                    <span id="humidity-number">28</span>%
-                  </strong>
-                </div>
-              </div>
-              <br />
-              <div className="weather-forecast" id="forecast"></div>
-            </div>
+            <WeatherForecast />
           </div>
-          <WeatherForecast />
         </div>
-      </div>
 
-      <script src="script.js"></script>
+        <script src="script.js"></script>
+      </div>
     </div>
   );
 }
